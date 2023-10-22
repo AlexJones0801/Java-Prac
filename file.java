7d02cc486c33e800399c5b84c6e3ef64bc0a695a
AlexJones0801 <shivanshkurmi2@gmail.com>
2023-10-22 16:10:16 +0530

    alex

---
+++ file.java
@@ -0,0 +1,26 @@
+import java.io.*;
+class file {
+   public static void main(String args[]) {
+      File inFile = new File("input.txt");
+      File outFile = new File("output.txt");
+      FileReader ins = null;
+      FileWriter outs = null;
+      try{
+         ins = new FileReader(inFile);
+         outs = new FileWriter(outFile);
+         int ch;
+         while((ch = ins.read()) !=-1){
+               outs.write(ch);
+         }
+      } catch(IOException e) {
+         System.out.println(e);
+         System.exit(-1);
+      }
+      finally{
+         try{
+            ins.close();
+            outs.close();
+         } catch(IOException e){}
+      }
+   }
+}
